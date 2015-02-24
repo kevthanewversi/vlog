7c495309d0fb1be1830bd953c9e492170a8e6a60
kevthanewversi <murimiclerk@gmail.com>
2015-02-24 14:00:50 +0300

    added paperclip for uploading of images

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -70,6 +70,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:title, :length, :star, :description)
+      params.require(:movie).permit(:title, :length, :star, :description,:image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224083552) do
+ActiveRecord::Schema.define(version: 20150224094518) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.time     "length"
     t.string   "star"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
