f229ddfd8d50f37e1598b464d99815f67e42209d
kevthanewversi <murimiclerk@gmail.com>
2015-03-25 13:02:12 +0300

    changed reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
 
   devise_for :users
-  resources :movies
-
+  resources :movies do 
+  resources :reviews, except: [:show, :index]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226114358) do
+ActiveRecord::Schema.define(version: 20150325085204) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150226114358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
