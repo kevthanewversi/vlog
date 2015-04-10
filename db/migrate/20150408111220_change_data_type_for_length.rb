1103dfbd97c174ec6ea2846a6c8e4c5892acd3e8
kevthanewversi <murimiclerk@gmail.com>
2015-04-10 14:58:37 +0300

    changed change_data_type_for_length migration file

--- db/migrate/20150408111220_change_data_type_for_length.rb
+++ db/migrate/20150408111220_change_data_type_for_length.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypeForLength < ActiveRecord::Migration
  def change
- 	change_column :movies, :length, :float 
+ 	change_column :movies, :length, 'float USING CAST(length AS float)'
  end
 end
